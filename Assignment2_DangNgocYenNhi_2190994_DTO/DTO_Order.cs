89e7e88108bee2aedd3f902f8d688c43a685e559
Yen Nhi <nhinhi.04022001@gmail.com>
2022-08-09 14:05:31 +0700

    DTO

---
+++ Assignment2_DangNgocYenNhi_2190994_DTO/DTO_Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_DangNgocYenNhi_2190994_DTO
+{
+    public class DTO_Order
+    {
+        public int IdDetails { get; set; }
+        public string NameDetails { get; set; }
+        public int Quantity { get; set; }
+        public decimal Totail { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DTO_Order() { }
+
+    }
+}
